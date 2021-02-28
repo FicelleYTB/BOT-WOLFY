049e2071f4aefd7641ae9653cfd977bf9c220591
FicelleYTB <59775826+FicelleYTB@users.noreply.github.com>
2021-02-28 13:13:45 +0100

    Add files via upload

--- src/fr/ficelleytb/main/Main.java
+++ src/fr/ficelleytb/main/Main.java
@@ -1,6 +1,7 @@
 package fr.ficelleytb.main;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -31,21 +32,38 @@ public class Main {
             PassloginForm.sendKeys(LoginPass);
             pressAnyKeyToContinue();
             Thread.sleep(600);
-            System.out.println("Ok !");
-            driver.findElement(By.className("Home_card__o_Q5X Home_instantPlay__3m-iu")).click();
+            System.out.println("[BOT] Ok, im ready :) !");
+            Thread.sleep(500);
+            ToggleChat();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    private static void pressAnyKeyToContinue()
-    {
-        System.out.println("Press Enter key to continue... (complete the captcha first)");
+    private static void pressAnyKeyToContinue() {
+        System.out.println("Press Enter key to continue... (connect to your account and go to in game)");
         try
         {
             System.in.read();
         }
         catch(Exception e)
         {}
+    }
+
+    private static void ToggleChat() {
+        try {
+            WebElement Chat = driver.findElement(By.tagName("textarea"));
+            Chat.sendKeys("Yo les mecs ça va ?" + Keys.ENTER);
+            Thread.sleep(100);
+            Chat.sendKeys("Moi ça va si on me demande en avance" + Keys.ENTER);
+            Thread.sleep(100);
+            Chat.sendKeys("Je parie que ça va être une bonne game pas vous?" + Keys.ENTER);
+            Thread.sleep(100);
+            Chat.sendKeys("Waw ça serais cool si ça commence mtn" + Keys.ENTER);
+            Thread.sleep(100);
+            Chat.sendKeys("Et si je vous dis que je n'ai pas écris ce message mais c'est un bout de code en java :o" + Keys.ENTER);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
